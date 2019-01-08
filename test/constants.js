5cc8f60df92eae43b53b5a21a56cbe5fd06e44d6
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2019-01-08 14:17:03 -0500

    enable npm test

---
+++ test/constants.js
@@ -0,0 +1,17 @@
+/**
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which accompanies this
+ * distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+const GITHUB_SECRET_PASSWORD = 'GSR_TEST_GITHUB_PASSWORD';
+const GITHUB_SECRET_TOKEN = 'GSR_TEST_GITHUB_TOKEN';
+const GITHUB_SECRET_USERNAME = 'GSR_TEST_GITHUB_USERNAME';
+
+module.exports = {
+  GITHUB_SECRET_PASSWORD,
+  GITHUB_SECRET_TOKEN,
+  GITHUB_SECRET_USERNAME,
+};
---
+++ test/test-version.js
@@ -0,0 +1,29 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+const expect = require('chai').expect;
+const debug = require('debug')('test:cli:version');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+describe('cli version', function() {
+  it('command should return version without error', async function() {
+    console.log('SHOW ENVIRONMENT VARIABLES START =============');
+    console.log(process.env);
+    console.log('SHOW ENVIRONMENT VARIABLES END =============');
+    const result = await exec('gsr --version');
+
+    debug('result:', result);
+
+    expect(result).to.have.property('stdout');
+    expect(result).to.have.property('stderr');
+
+    expect(result.stderr).to.be.empty;
+    expect(result.stdout.trim()).to.match(/^[0-9]+\.[0-9]+\.[0-9]+$/);
+  });
+});
